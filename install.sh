b18cb5965d6dd1e46d70faf3cc56a75ad647cd0d
leixd1994 <lxd@stu.ouc.edu.cn>
2019-06-04 08:23:41 +0800

    add install

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-echo 60 > /sys/class/backlight/intel_backlight/brightness
+echo 'Set the Current Permission '
+sudo chmod 646 /sys/class/backlight/intel_backlight/brightness 
+chmod +x ./higer_brightness
+chmod +x ./lower_brightness
+sudo cp ./higer_brightness /usr/bin
+sudo cp ./lower_brightness /usr/bin
